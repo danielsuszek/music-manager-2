c91b373f3d1c6cb79629ab6cfcd662f1b563e39c
danielsuszek <danielsuszek@gmail.com>
2017-12-05 20:11:54 +0100

    Add home page

---
+++ src/MusicManager/ManageBundle/Controller/HomePageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MusicManager\ManageBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomePageController extends Controller
+{
+    public function indexAction()
+    {
+//        return $this->render('MusicManagerManageBundle:HomePage:index.html.twig', array(
+//        ));
+        return $this->render('homepage/index.html.twig');
+
+    }
+
+}
